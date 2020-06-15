af005c81cc315aaee9311aa8489a5c5d2a8bf8be
Joe Gitonga <thekiharani@gmail.com>
2020-06-15 10:03:15 +0300

    models modifications

--- attendance/models.py
+++ attendance/models.py
@@ -8,14 +8,26 @@ class Course(models.Model):
     instructor = models.ForeignKey(get_user_model(), related_name='courses_instructed', on_delete=models.CASCADE)
     description = models.TextField(_('Course Description'), null=True, blank=True)
 
+    def save(self, request, *args, **kwargs):
+        self.instructor = request.user
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = _("Course")
+        verbose_name_plural = _("Courses")
+
     def __str__(self):
         return self.name
 
 class Enrollement(models.Model):
     student = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     course = models.ForeignKey('attendance.Course', on_delete=models.CASCADE)
-    date_enrolled = models.DateField(_('Date Enrolled'))
+    date_enrolled = models.DateTimeField(_('Date Enrolled'), auto_now_add=True)
     final_grade = models.CharField(max_length=1, blank=True, null=True)
+
+    class Meta:
+        verbose_name = _("Enrollement")
+        verbose_name_plural = _("Enrollement")
 
     def __str__(self):
         return f'{self.course} Enrollement'
@@ -23,6 +35,10 @@ class Enrollement(models.Model):
 class Lesson(models.Model):
     course = models.ForeignKey('attendance.Course', on_delete=models.CASCADE)
     lesson_time = models.DateTimeField(_('Lesson Time'))
+
+    class Meta:
+        verbose_name = _("Lesson")
+        verbose_name_plural = _("Lessons")
 
     def __str__(self):
         return f'{self.course} Lesson'
